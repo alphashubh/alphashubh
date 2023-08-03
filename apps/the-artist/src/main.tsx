89f4b6ae0cfa766aba8430628999ffcb5effd508
alphashubh <shubhamdwivedi747@gmail.com>
2023-08-03 16:16:50 +0530

    changes

--- apps/the-artist/src/main.tsx
+++ apps/the-artist/src/main.tsx
@@ -6,8 +6,8 @@ import App from './app/app';
 
 ReactDOM.render(
   <StrictMode>
-     <BrowserRouter basename={"/alphashubh/"}>
-      <Route path="/" Component={App} />
+     <BrowserRouter basename={process?.env?.['PUBLIC_URL'] + '/'}>
+      <Route path="/alphashubh" Component={App} />
      </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
