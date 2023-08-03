ac0081a1c2f21446d7c8f030291801dc34299ebd
alphashubh <shubhamdwivedi747@gmail.com>
2023-08-03 09:36:06 +0530

    update few things

--- apps/the-artist/src/main.tsx
+++ apps/the-artist/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 import App from './app/app';
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+     <BrowserRouter basename={"/alphashubh/"}>
+      <Route path="/" Component={App} />
+     </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
 );
